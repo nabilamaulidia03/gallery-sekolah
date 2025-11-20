8fe1f91e629dff9b1b20d7995bf4e3c2efc61519
nabilamaulidia03 <145429168+nabilamaulidia03@users.noreply.github.com>
2025-11-20 19:54:35 +0700

    Update Announcement.php

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -2,19 +2,29 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Announcement extends Model
 {
-    use HasFactory, HasUlids;
+    use HasFactory, HasUlids, Sluggable;
 
     public $incrementing = false;
     protected $keyType = 'string';
 
     protected $fillable = [
-        'title', 'content'
+        'title', 'content', 'image', 'slug'
     ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
 

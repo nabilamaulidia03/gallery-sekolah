d0bfc4787d3106dd53781512efcb49c5916632df
nabilamaulidia03 <145429168+nabilamaulidia03@users.noreply.github.com>
2025-11-20 19:54:49 +0700

    Update News.php

--- app/Models/News.php
+++ app/Models/News.php
@@ -2,18 +2,28 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
-    use HasFactory, HasUlids;
+    use HasFactory, HasUlids, Sluggable;
 
     public $incrementing = false;
     protected $keyType = 'string';
 
     protected $fillable = [
-        'title', 'content'
+        'title', 'content', 'image', 'slug'
     ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }

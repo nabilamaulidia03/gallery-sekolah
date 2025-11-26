483032861280df3aee34e79c214fd9fad31f084e
nabilamaulidia03 <145429168+nabilamaulidia03@users.noreply.github.com>
2025-11-26 12:28:46 +0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -90,6 +90,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
             Route::resource('gallery-categories', GalleryCategoryController::class);
             Route::get('messages', [ContactMessageController::class, 'index'])->name('messages.index');
             Route::get('messages/{message}', [ContactMessageController::class, 'show'])->name('messages.show');
+            Route::delete('messages/{message}', [ContactMessageController::class, 'destroy'])->name('messages.destroy');
             Route::resource('events', EventController::class);
             Route::resource('announcements', AnnouncementController::class);
             Route::resource('news', NewsController::class);

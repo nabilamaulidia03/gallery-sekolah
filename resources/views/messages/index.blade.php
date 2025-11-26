44b31b413c64ceccc6a7fdf7990ef43e26130920
nabilamaulidia03 <145429168+nabilamaulidia03@users.noreply.github.com>
2025-11-26 12:28:03 +0700

    Update

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -31,6 +31,15 @@
                             <a href="{{ route('admin.messages.show', $message->id) }}" class="btn btn-sm btn-primary">
                                 <i class="mdi mdi-eye"></i>
                             </a>
+                            <form action="{{ route('admin.messages.destroy', $message->id) }}" 
+                                method="POST" 
+                                class="d-inline-block delete-form">
+                                @csrf
+                                @method('DELETE')
+                                <button type="button" class="btn btn-sm btn-danger delete-button">
+                                    <i class="mdi mdi-trash-can"></i>
+                                </button>
+                            </form>
                         </td>
                     </tr>
                 @empty
@@ -42,4 +51,42 @@
         </table>
     </div>
 </div>
+@endsection
+
+@section('js')
+@if (session('success'))
+<script>
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: "{{ session('success') }}",
+        showConfirmButton: false,
+        timer: 1500,
+        showCloseButton: true
+    });
+</script>
+@endif
+
+<script>
+document.querySelectorAll('.delete-button').forEach(button => {
+    button.addEventListener('click', function (e) {
+        let form = this.closest('form');
+
+        Swal.fire({
+            title: 'Apakah anda yakin?',
+            text: "Sekali hapus, data ga bisa balik!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#6c757d',
+            confirmButtonText: 'Iya, hapus!',
+            cancelButtonText: 'Batal'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                form.submit();
+            }
+        });
+    });
+});
+</script>
 @endsection
